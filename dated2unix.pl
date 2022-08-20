30e5f80ff2adaef64423cb14ed88b352b57eb281
krzysckh <krzych@420blaze.it>
2022-08-20 22:27:07 +0200

    dated2unix.pl

---
+++ dated2unix.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+# prints the unix timestamp for images saved by my phone
+# mainly for img.krzysckh.org
+#
+# :^)
+#
+# krzysckh 2022
+# krzysckh.org
+
+use strict;
+use warnings;
+
+use File::Basename;
+use Time::Piece;
+
+my $fname;
+my $t;
+
+die "usage: $0 /path/to/IMG_...." if not defined $ARGV[0];
+
+$fname = $ARGV[0];
+
+die "$fname is not a file" if not -f $fname;
+
+$fname = basename($fname);
+
+die "basename not in right format" if not $fname=~m/IMG_[0-9]{8}_[0-9]{9}\..*/;
+
+$fname =~ s/...\..*//;
+$fname =~ s/IMG//;
+$fname =~ s/_//;
+
+$t = Time::Piece->strptime($fname, "%Y%m%d_%H%M%S");
+print $t->epoch, "\n";

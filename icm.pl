9a858ccdeb36dd04464fbd2651b1a91a80b4d478
krzysckh <krzych@420blaze.it>
2023-07-19 17:15:21 +0200

    huh

---
+++ icm.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+# pogoda z icmu
+#
+# krzysckh 2023
+# krzysckh.org
+
+use strict;
+use warnings;
+
+use HTTP::Tiny;
+
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) =
+  localtime(time);
+
+# magiczne numerki, (Kraków, Polska)
+# http://meteo.icm.edu.pl/
+my ($x, $y) = (466, 232);
+
+$mon  = sprintf("%02d", 1 + $mon);
+$mday = sprintf("%02d", $mday);
+$year += 1900;
+
+my $c = HTTP::Tiny->new;
+my $res = $c->get("http://meteo.icm.edu.pl/um/metco/mgram_pict.php"
+  . "?ntype=0u&fdate=$year$mon$mday" . "06&row=$x&col=$y&lang=pl");
+
+print $res->{content};
